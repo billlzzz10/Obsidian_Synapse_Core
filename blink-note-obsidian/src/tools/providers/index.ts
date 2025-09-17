31c33520dedcc4671474646baad8ebf0a101467d
billlzzz10 <billlzzz10@gmail.com>
2025-09-18 00:25:34 +0700

    Update index.ts

--- blink-note-obsidian/src/tools/providers/index.ts
+++ blink-note-obsidian/src/tools/providers/index.ts
@@ -7,3 +7,6 @@ export * from './Context7Provider'; // Placeholder for the new provider
 export * from './RemoteAdapter';
 export * from './WebDAVAdapter';
 export * from './S3Adapter';
+
+
+export * from './FetchProvider';
